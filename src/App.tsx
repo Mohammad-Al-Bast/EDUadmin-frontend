36eae0f0f2e20bb6d7ffe5293a07e62da1cd2234
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-07-19 17:24:11 +0300

    Install and configure shadcn/ui + add all needed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./components/ui/button"
+
 function App() {
   return (
     <div className="text-red-500">
       Hello, World!
+
+      <Button>
+        Click Me
+      </Button>
     </div>
   )
 }
