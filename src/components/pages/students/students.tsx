c9b6f8e5f505c06a34ded74e0dbf8d8f97b7f210
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-07-27 21:23:05 +0300

    Refactor import statements in StudentsPage for cleaner code structure

--- src/components/pages/students/students.tsx
+++ src/components/pages/students/students.tsx
@@ -1,5 +1,5 @@
-import { columns } from "@/components/dashboard/students/columns"
-import { DataTable } from "@/components/dashboard/students/data-table"
+import { columns } from "@/components/dashboard/students"
+import { DataTable } from "@/components/dashboard/students"
 import { studentsData } from "@/constants/students"
 
 export default function StudentsPage() {
